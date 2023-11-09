7649435568830e95c7f7ff54fd0ece245c38edfa
iusmar.lng@gmail.com <iusmar.lng@gmail.com>
2023-11-09 18:29:46 +0100

    Add project files.

---
+++ BattleshipLibrary/Models/Enums.cs
@@ -0,0 +1,14 @@
+﻿namespace BattleshipLibrary.Models
+{
+    public partial class GridSpotModel
+    {
+        public enum GridSpotStatus
+        {
+            Empty = 0,
+            Ship,
+            Miss,
+            Hit,
+            Sunk
+        }
+    }
+}
